9803f371acf9864d5e8ffab7ec62845afe938578
= <abdullaothman17@gmail.com>
2025-09-18 10:45:00 +0300

    fixing properties swipers

--- resources/views/components/properties/properties-swiper.blade.php
+++ resources/views/components/properties/properties-swiper.blade.php
@@ -30,10 +30,10 @@
                             class="flex flex-col items-center justify-center p-6 bg-[#0A2C73] text-white rounded-lg shadow-lg transition-transform hover:scale-105">
                             <div class="relative w-28 h-56 mb-4">
                                 <div class="absolute inset-0 bg-cover bg-center opacity-70 rounded-md"
-                                    style="background-image: url('https://www.tira.go.tz/uploads/services/a4e9a3a9a6d71c0a2440aab1d42e62ca.jpeg'); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
+                                    style="background-image: url(''); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
                                 </div>
                                 <div class="absolute inset-0 flex items-center justify-center">
-                                    <img src="https://www.tira.go.tz/uploads/services/icon-1659878969" alt="ORS Icon"
+                                    <img src="" alt="ORS Icon"
                                         class="w-10 h-10">
                                 </div>
                             </div>
@@ -49,14 +49,14 @@
                             class="flex flex-col items-center justify-center p-6 bg-[#0A2C73] text-white rounded-lg shadow-lg transition-transform hover:scale-105">
                             <div class="relative w-28 h-56 mb-4">
                                 <div class="absolute inset-0 bg-cover bg-center opacity-70 rounded-md"
-                                    style="background-image: url('https://www.tira.go.tz/uploads/services/a4e9a3a9a6d71c0a2440aab1d42e62ca.jpeg'); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
+                                    style="background-image: url(''); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
                                 </div>
                                 <div class="absolute inset-0 flex items-center justify-center">
-                                    <img src="https://www.tira.go.tz/uploads/services/icon-1659885934" alt="RBS Icon"
+                                    <img src="" alt="RBS Icon"
                                         class="w-10 h-10">
                                 </div>
                             </div>
-                            <h3 class="font-semibold text-lg mt-2">ZHC Online Portal</h3>
+                            <h3 class="font-semibold text-lg mt-2">Online Portal</h3>
                         </div>
                     </a>
                 </div>
@@ -71,7 +71,7 @@
                                     style="background-image: url(''); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
                                 </div>
                                 <div class="absolute inset-0 flex items-center justify-center">
-                                    <img src="https://www.tira.go.tz/uploads/services/icon-1659878879" alt="MIS Icon"
+                                    <img src="" alt="MIS Icon"
                                         class="w-10 h-10">
                                 </div>
                             </div>
@@ -89,7 +89,7 @@
                                     style="background-image: url(''); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
                                 </div>
                                 <div class="absolute inset-0 flex items-center justify-center">
-                                    <img src="https://www.tira.go.tz/uploads/services/icon-1659878879" alt="MIS Icon"
+                                    <img src="" alt="MIS Icon"
                                         class="w-10 h-10">
                                 </div>
                             </div>
@@ -107,7 +107,7 @@
                                     style="background-image: url(''); clip-path: polygon(50% 0%, 93% 25%, 93% 75%, 50% 100%, 7% 75%, 7% 25%);">
                                 </div>
                                 <div class="absolute inset-0 flex items-center justify-center">
-                                    <img src="https://www.tira.go.tz/uploads/services/icon-1659878879" alt="MIS Icon"
+                                    <img src="" alt="MIS Icon"
                                         class="w-10 h-10">
                                 </div>
                             </div>
