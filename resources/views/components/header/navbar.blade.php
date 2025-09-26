903ae6932ad2ba050ba5e746235d738fb5e2568d
= <abdullaothman17@gmail.com>
2025-09-26 16:04:23 +0300

    upcoming project made

--- resources/views/components/header/navbar.blade.php
+++ resources/views/components/header/navbar.blade.php
@@ -1,6 +1,7 @@
 @php
     $completedProjects = App\Models\Project::where('status', 'Completed')->get();
     $ongoingProjects = App\Models\Project::where('status', 'Ongoing')->get();
+    $upcomingProjects = App\Models\Project::where('status', 'Upcoming')->get();
 @endphp
 <nav {{ $attributes->merge(['class' => 'bg-[#F7931E] text-white']) }}>
     <div class="container  justify-center mx-auto px-6 py-4 space-x-4 hidden md:flex">
@@ -113,6 +114,37 @@
                             class="absolute top-0 left-full ml-1 w-48 bg-[#0A2C73] rounded shadow-lg z-30">
                             <ul class="py-1">
                                 @foreach ($ongoingProjects as $project)
+                                    <li><a href="{{ route('projects.show', [$project->status, $project->slug]) }}"
+                                            class="block px-4 py-2 hover:bg-[#123b9b] text-sm font-medium">
+                                            {{ $project->title }}
+                                        </a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </li>
+                    <!-- Upcoming Projects -->
+                    <li x-data="{ subOpen: false }" class="relative">
+                        <button @click="subOpen = !subOpen" @keydown.escape="subOpen = false"
+                            class="w-full flex justify-between items-center px-4 py-2 text-sm font-medium hover:bg-[#123b9b]">
+                            Upcoming
+                            <svg class="w-4 h-4 transition-transform duration-200" :class="{ 'rotate-90': subOpen }"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 5l7 7-7 7" />
+                            </svg>
+                        </button>
+
+                        <div x-show="subOpen" x-cloak @click.away="subOpen = false"
+                            x-transition:enter="transition ease-out duration-200"
+                            x-transition:enter-start="opacity-0 -translate-x-2"
+                            x-transition:enter-end="opacity-100 translate-x-0"
+                            x-transition:leave="transition ease-in duration-150"
+                            x-transition:leave-start="opacity-100 translate-x-0"
+                            x-transition:leave-end="opacity-0 -translate-x-2"
+                            class="absolute top-0 left-full ml-1 w-48 bg-[#0A2C73] rounded shadow-lg z-30">
+                            <ul class="py-1">
+                                @foreach ($upcomingProjects as $project)
                                     <li><a href="{{ route('projects.show', [$project->status, $project->slug]) }}"
                                             class="block px-4 py-2 hover:bg-[#123b9b] text-sm font-medium">
                                             {{ $project->title }}
