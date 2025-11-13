29784a5c50a37dd58958944616d5f5120d2b51b6
babaman <abdullaothman17@gmail.com>
2025-11-13 08:49:46 +0300

    working with production pro footer link

--- resources/views/components/footer/contact.blade.php
+++ resources/views/components/footer/contact.blade.php
@@ -11,7 +11,7 @@
 
         <div class="w-full md:w-5/12 pl-0 md:pl-6 text-white text-center sm:text-center md:text-left">
             <x-footer.heading :title="__('landing.landing.properties')" />
-            @foreach ([['label' => 'Home', 'url' => '/'], ['label' => 'Contact Details', 'url' => '/contacts'], ['label' => 'Mission and Vision', 'url' => '/about'], ['label' => 'FAQs', 'url' => '/faqs'], ['label' => 'What we do?', 'url' => '/about/about_us'], ['label' => 'Who we Are?', 'url' => '/about/about_us'], ['label' => 'Who we Serve?', 'url' => '/about/about_us']] as $link)
+            @foreach ([['label' => 'Home', 'url' => '/'], ['label' => 'Contact Details', 'url' => '/contacts'], ['label' => 'Mission and Vision', 'url' => '/about_us'], ['label' => 'FAQs', 'url' => '/faqs'], ['label' => 'What we do?', 'url' => '/about/about_us'], ['label' => 'Who we Are?', 'url' => '/about/about_us'], ['label' => 'Who we Serve?', 'url' => '/about/about_us']] as $link)
                 <div class="py-1"><a href="{{ $link['url'] }}" class="hover:underline">{{ $link['label'] }}</a></div>
             @endforeach
         </div>
